ab3f16aa7e7db73ca60204498fae24e1d8072a5e
Samantha Young <samantha.young@students.olin.edu>
2017-09-17 09:01:37 -0400

    fix style

--- weapp_gifspeak.py
+++ weapp_gifspeak.py
@@ -10,9 +10,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def starting_page(top_story1=None):
-    
+
     top_story1 = text_input(url1=None,url2=None,url3=None,url4=None,url5=None)
     return render_template('home.html', top_story1=top_story1)
+
+@app.route('/test')
+def test():
+    return render_template('newHome.html')
 
 @app.route('/about_us')
 def about_team():
